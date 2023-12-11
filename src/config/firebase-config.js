c05f1f72c6854dc516d2505003e68c4daa6031fa
Ivan <ivanillera@gmail.com>
2023-12-11 17:23:23 -0300

    update 5 files and delete 4 files

---
+++ src/config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAonf_hP4I0CCr4FYhXlfr1lDnk9MGBoUg",
+  authDomain: "expense-tracker-c1395.firebaseapp.com",
+  projectId: "expense-tracker-c1395",
+  storageBucket: "expense-tracker-c1395.appspot.com",
+  messagingSenderId: "244820525850",
+  appId: "1:244820525850:web:68cb02dcf7efcaf125e2a3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/auth/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Auth = () => {
+  return (
+<div class="px-6 sm:px-0 max-w-sm">
+<button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+</div>
+  )
+}
